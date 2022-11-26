1dfb3728ae28308ed0df316cc95a779b093b106d
rdoby99 <red55916@uga.edu>
2022-11-25 22:45:35 -0500

    Button

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Button = (props) => {
-    return (
-        <div>
-            <button className="inline-block border-2 border-white py-4 px-8 rounded-full bg-black/50 hover:bg-white hover:text-black transition duration-200">{props.text}</button>
-        </div>
-    );
+  return (
+    <div>
+      <button className="inline-block border-2 border-white py-4 px-8 rounded-full bg-black/50 hover:bg-white hover:text-black transition duration-200">{props.text}</button>
+    </div>
+  );
 };
 
 export default Button;
